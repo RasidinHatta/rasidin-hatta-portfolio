ba184a2c7c67499d659a9ad4714bc849c3c4d743
RasidinHatta <129116066+RasidinHatta@users.noreply.github.com>
2025-12-31 14:33:53 +0800

    change dir for light pillar

--- components/animation/AnimatedBackground.tsx
+++ components/animation/AnimatedBackground.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import Iridescence from "@/components/react-bits/Iridescence";
 import FloatingLines from "@/components/react-bits/FloatingLines";
-import LightPillar from "../LightPillar";
+import LightPillar from "../react-bits/LightPillar";
 
 export default function AnimatedBackground() {
   const { resolvedTheme } = useTheme();
