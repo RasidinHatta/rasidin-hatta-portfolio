8373ba3978390a535a3793a515dde71baab98338
RasidinHatta <129116066+RasidinHatta@users.noreply.github.com>
2026-04-21 12:33:24 +0800

    edit mpl-tracker link

--- data/project.ts
+++ data/project.ts
@@ -84,7 +84,7 @@ export const projects: Project[] = [
             "Focus on making coding enjoyable and interactive"
         ],
         githubUrl: "https://github.com/RasidinHatta/mpl-tracker",
-        liveUrl: "https://mpl-tracker.vercel.app",
+        liveUrl: "https://mpl-tracker-taupe.vercel.app",
         period: "Apr 2026 - Present",
         team: "Solo Project",
         type: "personal",
